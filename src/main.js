5b7e5b65d2662ae10249ac588405b8230318d29c
Rezzvy ⸜(｡˃ ᵕ ˂ )⸝♡ <rezzvy@hotmail.com>
2025-01-01 23:15:13 +0800

    feat: implement canvas item addition and functionality

--- src/main.js
+++ src/main.js
@@ -1,15 +1,61 @@
 const canvasElementListContainer = document.getElementById("canvas-element-list");
 const canvasElementModalEditContainer = new bootstrap.Modal("#modal-edit");
 const canvasElementModalEditBody = document.getElementById("modal-edit-body");
-const canvasElement = document.getElementById("canvas");
+const canvasElement = document.getElementById("canvas-wrapper");
 
-new bootstrap.Tooltip(document.body, {
+const modalRenderLargeList = ["imgmap", "text", "codeblock"];
+
+new bootstrap.Tooltip(document.querySelector("main"), {
   selector: '[data-bs-toggle="tooltip"]',
   trigger: "hover",
 });
 
+new bootstrap.Tooltip(canvasElementModalEditBody, {
+  selector: '[data-bs-toggle="tooltip"]',
+  trigger: "hover",
+});
+
+let currentActionKey = "";
+
+const drake = dragula({
+  isContainer: (el) => {
+    return el.hasAttribute("data-drop") || el === canvasElement;
+  },
+  moves: (el, source, handle) => {
+    return handle.closest('[data-action="move"]');
+  },
+  accepts: (el, target) => {
+    if (el.querySelector("center") && target.closest("center")) {
+      return false;
+    }
+
+    if (el.querySelector(".notice") && target.closest(".notice")) {
+      return false;
+    }
+
+    if (el.contains(target)) {
+      return false;
+    }
+
+    return true;
+  },
+  revertOnSpill: true,
+});
+
+drake.on("drop", (el, target, source, sibling) => {
+  if (target.classList.contains("ph")) {
+    target.classList.remove("ph");
+  }
+
+  if (source.children.length === 0) {
+    source.classList.add("ph");
+  }
+});
+
 let pickr = null;
 let quill = null;
+
+const skeleton = {};
 
 function generateCanvasItem(key, content) {
   const div = document.createElement("div");
@@ -28,16 +74,16 @@ function generateCanvasItem(key, content) {
     </div>
 
     <div>
-      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Move">
+      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Move" data-action="move">
         <i class="fa fa-arrows fa-fw"></i>
       </button>
-      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Edit">
+      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Edit" data-action="edit" data-key="${key}">
         <i class="fa fa-pencil fa-fw"></i>
       </button>
-      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Duplicate">
+      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Duplicate" data-action="duplicate">
         <i class="fa fa-copy fa-fw"></i>
       </button>
-      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Remove">
+      <button class="btn btn-outline-light p-0 border-0" data-bs-toggle="tooltip" data-bs-title="Remove" data-action="remove">
         <i class="fa fa-trash fa-fw"></i>
       </button>
     </div>
@@ -54,13 +100,13 @@ function generateCanvasItem(key, content) {
 async function generateCanvasElementList(canvasElementList) {
   const buttonFragment = document.createDocumentFragment();
   const modalFragment = document.createDocumentFragment();
-  const canvasFragment = document.createDocumentFragment();
 
   for (const key in canvasElementList) {
     const button = document.createElement("button");
     button.classList.add("btn", "btn-dark", "py-3", "px-5");
     button.dataset.bsToggle = "tooltip";
     button.dataset.bsTitle = canvasElementList[key].tooltipTitle;
+    button.dataset.key = key;
     button.innerHTML = `<i class="fa ${canvasElementList[key].icon} fa-fw fa-lg"></i>`;
     buttonFragment.appendChild(button);
 
@@ -69,19 +115,18 @@ async function generateCanvasElementList(canvasElementList) {
       const div = document.createElement("div");
       div.dataset.edit = key;
       div.innerHTML = content;
-
+      div.classList.add("d-none");
       modalFragment.appendChild(div);
     }
 
     if (canvasElementList[key].skeletonPath) {
       const content = await fetch(canvasElementList[key].skeletonPath).then((res) => res.text());
-      canvasFragment.appendChild(generateCanvasItem(key, content));
+      skeleton[key] = content;
     }
   }
 
   canvasElementModalEditBody.appendChild(modalFragment);
   canvasElementListContainer.appendChild(buttonFragment);
-  canvasElement.appendChild(canvasFragment);
 
   return true;
 }
@@ -132,3 +177,65 @@ window.addEventListener("scroll", (e) => {
 
 new ResizeObserver(updateStickyState).observe(canvas);
 loadCanvasElements();
+
+canvasElementListContainer.addEventListener("click", (e) => {
+  const elementListButton = e.target.closest("button[data-key]");
+
+  if (elementListButton) {
+    const key = elementListButton.dataset.key;
+    canvasElement.appendChild(generateCanvasItem(key, skeleton[key]));
+  }
+});
+
+canvasElement.addEventListener("click", (e) => {
+  const removeButton = e.target.closest('button[data-action="remove"]');
+  const duplicateButton = e.target.closest('button[data-action="duplicate"]');
+  const editButton = e.target.closest('button[data-action="edit"]');
+
+  if (removeButton) {
+    const tooltip = bootstrap.Tooltip.getInstance(removeButton);
+    if (tooltip) {
+      tooltip.dispose();
+    }
+
+    e.target.closest(".canvas-item").remove();
+    return;
+  }
+
+  if (duplicateButton) {
+    const copied = e.target.closest(".canvas-item").cloneNode(true);
+    e.target.closest(".canvas-item").parentElement.appendChild(copied);
+    return;
+  }
+
+  if (editButton) {
+    currentActionKey = editButton.dataset.key;
+    canvasElementModalEditContainer.show();
+    return;
+  }
+});
+
+canvasElementModalEditContainer._element.addEventListener("show.bs.modal", (e) => {
+  const modalDialogElement = e.target.querySelector(".modal-dialog");
+  if (modalRenderLargeList.includes(currentActionKey)) {
+    modalDialogElement.classList.add("modal-lg");
+  } else {
+    modalDialogElement.classList.remove("modal-lg");
+  }
+
+  const currentRenderedModal = canvasElementModalEditBody.querySelector(".d-block[data-edit]");
+
+  if (currentRenderedModal) {
+    currentRenderedModal.classList.replace("d-block", "d-none");
+  }
+
+  const currentActionModal = canvasElementModalEditBody.querySelector(`[data-edit="${currentActionKey}"]`);
+
+  if (currentActionModal) {
+    currentActionModal.classList.replace("d-none", "d-block");
+  }
+});
+
+canvasElementModalEditContainer._element.addEventListener("hide.bs.modal", () => {
+  currentActionKey = "";
+});
